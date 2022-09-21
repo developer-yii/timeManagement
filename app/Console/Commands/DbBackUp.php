1d2bcc9b7902e4f220dd511692d5eff665fefdfd
navnit.amcodr <navnit.amcodr@gmail.com>
2022-09-21 19:31:26 +0530

    db command change for backup

--- app/Console/Commands/DbBackUp.php
+++ app/Console/Commands/DbBackUp.php
@@ -39,11 +39,13 @@ class DbBackUp extends Command
      */
     public function handle()
     {
-        $filename = "backup-" . Carbon::now()->format('Y-m-d-H-i') . ".sql";
-        \Log::info($filename);
+        $filename = "backup-" . Carbon::now()->format('Y-m-d-H-i-s') . ".sql";        
 
         $file_path = storage_path() . "/app/public/db-backup/" . $filename;
-        $command = "mysqldump --user=" . env('DB_USERNAME') ." --password=" . env('DB_PASSWORD') . " --host=" . env('DB_HOST') . " " . env('DB_DATABASE') . " > " .$file_path;
+
+        \Log::info($file_path);
+        
+        $command = 'mysqldump --user=' . env('DB_USERNAME') .' --password="' . env('DB_PASSWORD') . '" --host=' . env('DB_HOST') . ' ' . env('DB_DATABASE') . ' > ' .$file_path;
 
         \Log::info($command);
 
@@ -54,9 +56,9 @@ class DbBackUp extends Command
 
         // Storage::disk('s3')->put("db-backup/".$filename, Storage::disk('public')->get('db-backup/' . $filename));
 
-        if(file_exists($file_path)) {
-            unlink($file_path);
-        }
+        // if(file_exists($file_path)) {
+        //     unlink($file_path);
+        // }
         return 0;
     }
 }
